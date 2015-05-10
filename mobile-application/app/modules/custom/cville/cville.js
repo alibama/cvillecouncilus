233dc7d55140c0536a9882a519b6661ba6e668cf
Anson Parker <ansondparker@gmail.com>
2015-05-10 15:42:41 -0400

    fixed the app, had messed up services, turned back on and added the code for cville.js

--- mobile-application/app/modules/custom/cville/cville.js
+++ mobile-application/app/modules/custom/cville/cville.js
@@ -50,4 +50,20 @@
     }
     catch (error) { console.log('cville_issue_list_empty - ' + error); }
   }
-
+function cville_form_alter(form, form_state, form_id) {
+  try {
+    if (form_id == 'node_edit' && form.elements.type.default_value == 'where_it_s_at') {
+      // The site is using Automatic node titles, disable access to the default
+      // title field and make it optional.
+      form.elements.title.access = false;
+      form.elements.title.required = false;
+      // Disable access to other fields not needed.
+      //form.elements.field_contest_entry.access = false;
+      //form.elements.field_place_finish.access = false;
+      //form.elements.field_addthis.access = false;
+      // Redirect the node edit form submission to the user's gallery.
+      // form.action = 'gallery/my';
+      }
+     }
+    catch (error) { console.log('tree_form_error - ' + error); }
+}
